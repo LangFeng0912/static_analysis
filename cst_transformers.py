94fd277d456ebdd652cef1672e799d2d938986cc
fenglang <Feng3133504033@outlook.com>
2023-04-28 13:41:12 +0200

    extract and reveal all the useful types

--- cst_transformers.py
+++ cst_transformers.py
@@ -15,6 +15,7 @@ class TypeAnnotationFinder(cst.CSTTransformer):
         }
     """
     METADATA_DEPENDENCIES = (PositionProvider,)
+
     def __init__(self):
         super().__init__()
         self.annotated_types = []
@@ -36,14 +37,15 @@ class TypeAnnotationFinder(cst.CSTTransformer):
             # print(param)
             if param.annotation is not None:
                 type_dict = dict(dt="param", func_name=original_node.name.value, name=param.name.value,
-                                 label=param.annotation.annotation.value)
+                                 label=param.annotation.annotation.value, loc=self.__get_line_column_no(param))
                 self.annotated_types.append(type_dict)
         for statement in original_node.body.body:
             if type(statement.body[0]) == cst.AnnAssign:
                 # print(statement.body[0])
                 # print(ranges[statement.body[0]].start)
                 type_dict = dict(dt="var", func_name=original_node.name.value, name=statement.body[0].target.value,
-                                 label=statement.body[0].annotation.annotation.value)
+                                 label=statement.body[0].annotation.annotation.value,
+                                 loc=self.__get_line_column_no(statement.body[0]))
                 self.annotated_types.append(type_dict)
                 self.var_list.add(self.__get_line_column_no(statement.body[0]))
 
@@ -51,14 +53,63 @@ class TypeAnnotationFinder(cst.CSTTransformer):
         pos = self.__get_line_column_no(node)
         if pos not in self.var_list:
             type_dict = dict(dt="var", func_name="__global", name=node.target.value,
-                                 label=node.annotation.annotation.value)
+                             label=node.annotation.annotation.value, loc=pos)
             self.annotated_types.append(type_dict)
 
-    # def visit_Param(self, original_node: cst.Param):
-    #     if original_node.annotation is not None:
-    #         print(f"{original_node.name.value}: {original_node.annotation.annotation.value}")
-    #         # self.annotated_params.append(original_node)
-    # def visit_AnnAssign(self, original_node: cst.AnnAssign):
-    #     if original_node.annotation is not None:
-    #         print(f"{original_node.target.value}: {original_node.annotation.annotation.value}")
+
+class TypeAnnotationMasker(cst.CSTTransformer):
+    """
+    It removes type annotations and add reveal_type() for static analysis
+    """
+    METADATA_DEPENDENCIES = (PositionProvider,)
+
+    def __init__(self, tar_dict):
+        super().__init__()
+        self.target = tar_dict
+        self.dt = tar_dict["dt"]
+        self.find = False
+
+    def __get_line_column_no(self, node):
+        lc = self.get_metadata(cst.metadata.PositionProvider, node)
+        return (lc.start.line, lc.start.column), (lc.end.line, lc.end.column)
+
+    def leave_FunctionDef(self, original_node: cst.FunctionDef, updated_node):
+
+        if self.find == False and original_node.name.value == self.target["func_name"]:
+            # for return types
+
+            if self.dt == "ret" and original_node.returns is not None:
+                log_stmt = cst.Expr(cst.parse_expression(f"reveal_type({updated_node.name.value})"))
+                return cst.FlattenSentinel([updated_node.with_changes(returns=None), log_stmt])
+
+            # for parameters
+            if self.dt == "param":
+                updated_params = []
+                for param in original_node.params.params:
+                    if param.name.value == self.target["name"]:
+                        param_untyped = cst.Param(param.with_changes(annotation=None, comma=None))
+                        updated_params.append(param_untyped)
+                    else:
+                        updated_params.append(param)
+                log_stmt = cst.Expr(cst.parse_expression(f"reveal_type({updated_node.name.value})"))
+                return cst.FlattenSentinel([updated_node.with_changes(
+                    params=cst.Parameters(updated_params)), log_stmt]
+                )
+
+        else:
+            return updated_node
+
+    def leave_AnnAssign(self, original_node: cst.AnnAssign, updated_node) -> None:
+        if self.find == False and self.dt == "var":
+            pos = self.__get_line_column_no(original_node)
+            print(pos)
+            if pos == self.target["loc"] and original_node.target.value == self.target["name"]:
+                self.find = True
+                log_stmt = cst.Expr(cst.parse_expression(f"reveal_type({updated_node.target.value})"))
+                updated_node = cst.Assign(targets=[cst.AssignTarget(target=original_node.target)],
+                                          value=original_node.value)
+                return cst.FlattenSentinel([updated_node, log_stmt])
+        else:
+            return updated_node
+
 
--- extract_types.py
+++ extract_types.py
@@ -1,11 +1,12 @@
 import libcst as cst
-from cst_transformers import TypeAnnotationFinder
+from cst_transformers import TypeAnnotationFinder, TypeAnnotationMasker
+import json
 
 code = """
 p: str = "hello"
 def my_function(x: int, y: float) -> int:
-    p: int = 5
     print("Hello, world!")
+    t = 3
     return 1
 """
 
@@ -13,10 +14,21 @@ def extract(code):
     parsed_program = cst.parse_module(code)
     transformer = TypeAnnotationFinder()
     new_tree = cst.metadata.MetadataWrapper(parsed_program).visit(transformer)
-    print(transformer.annotated_types)
+    # print(transformer.annotated_types)
+    with open('example_types.json', 'w') as f:
+        json.dump(transformer.annotated_types, f, indent = 4)
+    return transformer.annotated_types
 
-
+def mask_reveal(code,type):
+    parsed_program = cst.parse_module(code)
+    transformer_mask = TypeAnnotationMasker(type)
+    new_tree = cst.metadata.MetadataWrapper(parsed_program).visit(transformer_mask)
+    print(new_tree.code)
+    # print(transformer.annotated_types)
 
 if __name__ == '__main__':
-    extract(code)
-
+    type_list = extract(code)
+    for type in type_list:
+        print(type)
+        code_org = code
+        mask_reveal(code_org, type)
