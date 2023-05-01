fc93fdaa8c6e44e9d8a84f1944596ad736ecbe6c
fenglang <Feng3133504033@outlook.com>
2023-05-01 20:03:06 +0200

    extract and reveal all the useful types

--- utils.py
+++ utils.py
@@ -1,11 +1,13 @@
 import subprocess
 import json
 import re
+import os
+from os.path import join, exists, isdir
 
 var_pattern = r'Type of "(\w+)" is "(\w+)"'
 func_pattern = r'Type of "(.+)" is "(.+)"'
 param_pattern = r"(\w+): (\w+(?:\[.*?\])?)"
-
+desp_pattern = r'^Revealed type \[-1\]: Revealed type for `([\w\.]+)` is `(.+?)`(?: \(inferred: `(.+?)`\))?\.$'
 
 def run_command(cmd_args, timeout):
     process = subprocess.run(cmd_args, shell=True, capture_output=True, timeout=timeout)
@@ -52,6 +54,51 @@ def parse_pyright(p_dict, dt, name):
                 return None
         else:
             return None
+
+def parse_pyre(p_dict, dt, name):
+    description = p_dict["description"]
+    # print(description)
+    match = re.match(desp_pattern, description)
+    if match:
+        print(match.group(1))
+        print(match.group(2))
+    else:
+        print(description)
+
+
+def start_pyre(project_path):
+    if exists(join(project_path, '.watchmanconfig')):
+        os.remove(join(project_path, '.watchmanconfig'))
+    if exists(join(project_path, '.pyre_configuration')):
+        os.remove(join(project_path, '.pyre_configuration'))
+    pyre_dict = {
+        "site_package_search_strategy": "pep561",
+        "source_directories": [
+            "."
+        ],
+        "workers": 127,
+        "typeshed": "/pyre-check/stubs/typeshed/typeshed"
+    }
+    watchman_dict = {"root": "."}
+    with open(join(project_path, '.watchmanconfig'), "w") as f:
+        json.dump(watchman_dict, f)
+    with open(join(project_path, '.pyre_configuration'), "w") as f_pyre:
+        json.dump(pyre_dict, f_pyre)
+
+    stdout, stderr, r_code = run_command("cd %s; watchman watch-project ." % project_path, 60)
+    stdout_1, stderr_1, r_code_1 = run_command("cd %s; pyre start" % project_path, 60)
+    if r_code == 0 and r_code_1 == 0:
+        return True
+    else:
+        return False
+
+def pyre_infer(project_path, file_path, dt, name):
+    stdout, stderr, r_code = run_command("cd %s; pyre --output json" % project_path, 60)
+    output = json.loads(stdout)
+    for dict in output:
+        if dict["name"]=="Revealed type":
+            parse_pyre(dict, dt, name)
+
 
 
 def pyright_infer(file_path, dt: str, name: str):
